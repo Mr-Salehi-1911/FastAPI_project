f174cd3e6e2d414084a6851debc6d9c77a17361e
Iman <imanalonesalehi1911@gmail.com>
2025-12-28 20:06:38 +0330

    main file updated

--- core/main.py
+++ core/main.py
@@ -3,49 +3,50 @@ from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
-costs = [
-    {"id": 1, "page": "main_page", "cost": 5.5},
-    {"id": 2, "page": "products", "cost": 12.45},
-    {"id": 3, "page": "orders", "cost": 8.25},
-    {"id": 4, "page": "about us", "cost": 2.75},
-    {"id": 5, "page": "contact us", "cost": 3.75}
+# This is a list of dicts for creating a website that contains prices for each page to create.
+prices = [
+    {"id": 1, "page": "main_page", "price": 5.5},
+    {"id": 2, "page": "products", "price": 12.45},
+    {"id": 3, "page": "orders", "price": 8.25},
+    {"id": 4, "page": "about us", "price": 2.75},
+    {"id": 5, "page": "contact us", "price": 3.75}
 ]
 
-# ایجاد هزینه جدید با id یکتا
-@app.post("/costs", status_code= status.HTTP_201_CREATED)
-def create_cost(page_name:str, cost: float):
-    new_cost = {"id": len(costs) + 1, "page": page_name, "cost": cost}
-    costs.append(new_cost)
+# ایجاد هزینه جدید برای ساخت پیج جدید با شناسه آیدی یکتا
+@app.post("/prices", status_code= status.HTTP_201_CREATED)
+def create_price(page:str = Query(description="name of the page"), price: float = Query(description="price for create the page")):
+    new_price = {"id": len(prices) + 1, "page": page, "price": price}
+    prices.append(new_price)
 
-# دریافت همه ی هزینه ها
-@app.get("/costs")
-def retrieve_cost_list():
-    return costs
+# دریافت هزینه ی ساخت همه ی پیج ها
+@app.get("/prices")
+def retrieve_price_list():
+    return prices
 
-# دریافت یک هزینه با id هر page
-@app.get("/costs/{page_id}")
-def retrieve_cost_list(page_id: int):
-    for cost in costs:
-        if cost["id"] == page_id:
-            return JSONResponse(cost)
+# دریافت هزینه ی ساخت یک پیج
+@app.get("/prices/{id}")
+def retrieve_price_list(id: int):
+    for price in prices:
+        if price["id"] == id:
+            return JSONResponse(price)
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Object Not Found")
 
-# ویرایش هزینه ی یک page با شناسه ی id
-@app.put("/costs/{page_id}")
-def update_cost(page_id:int, new_cost:float):
-    for cost in costs:
-        if cost["id"] == page_id:
-            cost["cost"] = new_cost
-            message = f"Cost for page {cost["page"]} has changed into {cost["cost"]}"
+# ویرایش هزینه ی ساخت یک پیج با شناسه آیدی یکتا
+@app.put("/prices/{id}")
+def change_price(id:int, new_price:float = Path(description="add a new price for named page")):
+    for price in prices:
+        if price["id"] == id:
+            price["price"] = new_price
+            message = f"Price for page {price["page"]} has changed into {price["price"]} dollars"
             return {"message": message}
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Object Not Found")
 
-# حذف هزینه با id
-@app.delete("/costs/{page_id}", status_code= status.HTTP_204_NO_CONTENT)
-def delete_cost(page_id:int):
-    for cost in costs:
-        if cost["id"] == page_id:
-            message = f"Cost for page {cost["page"]} removed successfully"
-            costs.remove(cost)
+# حذف هزینه برای ساخت یک پیج با شناسه آیدی یکتا
+@app.delete("/prices/{id}", status_code= status.HTTP_204_NO_CONTENT)
+def delete_price(id:int = Path(description="insert id of related page to delete the price")):
+    for price in prices:
+        if price["id"] == id:
+            message = f"Price for page {price["page"]} removed successfully"
+            prices.remove(price)
             return JSONResponse({"message": message})
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Object Not Found")
